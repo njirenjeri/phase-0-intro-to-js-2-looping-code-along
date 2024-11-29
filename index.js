b6b28530031a5a288b39bdab87e589c8b0f85b47
Njire Njeri <njirenjeri@gmail.com>
2024-11-29 22:42:14 +0300

    looping lab complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const array = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(array){
+    let thankMessages = [];
+    for(let i = 0; i < array.length; i++ ){
+        thankMessages.push( `Thank you, ${array[i]}, for the wonderful surprise gift!`);
+    }
+    return thankMessages;
+}
+
+function countDown(num){
+    while(num >= 0){
+        console.log(num);
+        num --;
+    }
+}
